0710efea97b178fa135871de3a59f15d59760182
abraham konan kouame <abrahamkonany@gmail.com>
2023-02-13 18:13:51 +0000

    ajout
    code

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let scrollleft = document.getElementById("scrollleft");
+  let cardElement = document.getElementById("scrollElement");
+  let scrollright = document.getElementById("scrollright");
+  let position = 0;
+
+  scrollleft.addEventListener("click", (e) => {
+    e.preventDefault();
+    position = cardElement.scrollX;
+    cardElement.scroll(0, position);
+  });
+
+  scrollright.addEventListener("click", (e) => {
+    e.preventDefault();
+    position = cardElement.scrollX;
+    cardElement.scroll(position, 1000);
+  });
+
+  let icoclick = document.querySelectorAll(".fa-heart-o");
+  let icon = document.getElementById("icoclick");
+
+  icoclick.forEach((clic) => clic.addEventListener("click", (e)=>{
+   clic.classList.toggle('IconColor')
+
+  }));
+});
+
+// function  changIcon() {
+//     icoclick.color = '#F96E3F';
+//     icoclick.map()
+// }
