219311d2238cfc3651647daef087681fcd92c187
abraham konan kouame <abrahamkonany@gmail.com>
2023-02-15 17:49:42 +0000

    all boutique
    presque terminer

--- js/allBoutik.js
+++ js/allBoutik.js
@@ -1,16 +1,26 @@
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-  
-  let i;
-  let slides = document.querySelectorAll(".slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+document.addEventListener("DOMContentLoaded", () => {
+  let slideIndex = 0;
+  showSlides();
+  function showSlides() {
+    let i;
+    let slides = document.querySelectorAll(".slide");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+
+    let icoclick = document.querySelectorAll(".fa-heart-o");
+    let icon = document.getElementById("icoclick");
+
+    icoclick.forEach((clic) =>
+      clic.addEventListener("click", (e) => {
+        clic.classList.toggle("IconColor");
+      })
+    );
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 1000);
-}
+});
--- js/script.js
+++ js/script.js
@@ -18,14 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let icoclick = document.querySelectorAll(".fa-heart-o");
   let icon = document.getElementById("icoclick");
-
   icoclick.forEach((clic) =>
     clic.addEventListener("click", (e) => {
       clic.classList.toggle("IconColor");
     })
   );
 
-  let allcard = document.getElementsByClassName("allcard");
 });
 
 // function  changIcon() {
