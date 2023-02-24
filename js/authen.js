111803aaa9b5fb9e9879d21f89229c7f9aa8bdfa
abraham konan kouame <abrahamkonany@gmail.com>
2023-02-24 08:44:44 +0000

    Update authen.js

--- js/authen.js
+++ js/authen.js
@@ -6,26 +6,18 @@ function  verification() {
     let user = localStorage.getItem('user');
     user = JSON.parse(user);
     console.log(user)
-    if(user != null ){
-        user.forEach(cle => {
-            if(user === null){
-                userUtil.textContent=  "connexion";
-                userUtil.addEventListener("click", ()=>{
-                    window.location.href ='./page/login.html';
-                    
-                })
-            }
-            else{
-                data = localStorage.getItem('user');
-                data = JSON.parse(data);
-                data.forEach(element => {
-                    userUtil.textContent = element.nomPrenom;
-                    deco.style.opacity = 1;
+    if(user != null ){     
+        data = localStorage.getItem('user');
+        data = JSON.parse(data);
+        data.forEach(element => {
+        userUtil.textContent = element.nomPrenom;
+        deco.style.opacity = 1;
                 }); 
-            }  
-        }) 
     }
     else{
+        userUtil.textContent=  "connexion";
+        userUtil.addEventListener("click", ()=>{
+        window.location.href ='./page/login.html';})
         deco.style.opacity = 0;
     }
      
