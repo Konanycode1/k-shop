effbbc455311c9b6e541733c100ccf28cd146433
abraham konan kouame <abrahamkonany@gmail.com>
2023-02-24 08:39:11 +0000

    ok

--- js/authen.js
+++ js/authen.js
@@ -1,25 +1,34 @@
 let userUtil = document.getElementById('userUtil')
 let icon = '<i class="fa fa-user-circle-o" style="font-size:25px"></i>'
+let deco = document.getElementById("deco")
 
 function  verification() {
-    localStorage.getItem('user');
-    console.log(localStorage.getItem('user'))
-    if(localStorage.getItem('user') === null){
-        userUtil.textContent=  "connexion";
-        userUtil.addEventListener("click", ()=>{
-            window.location.href ='./page/login.html';
-        })
+    let user = localStorage.getItem('user');
+    user = JSON.parse(user);
+    console.log(user)
+    if(user != null ){
+        user.forEach(cle => {
+            if(user === null){
+                userUtil.textContent=  "connexion";
+                userUtil.addEventListener("click", ()=>{
+                    window.location.href ='./page/login.html';
+                    
+                })
+            }
+            else{
+                data = localStorage.getItem('user');
+                data = JSON.parse(data);
+                data.forEach(element => {
+                    userUtil.textContent = element.nomPrenom;
+                    deco.style.opacity = 1;
+                }); 
+            }  
+        }) 
     }
     else{
-        data = localStorage.getItem('user');
-        data = JSON.parse(data);
-       
-        data.forEach(element => {
-            userUtil.textContent = element.nomPrenom;
-        });
-        
-    }  
-    
+        deco.style.opacity = 0;
+    }
+     
 }
 verification();
 
